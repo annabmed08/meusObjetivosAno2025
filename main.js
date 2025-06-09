5b31ca94dc4ceb3d08e4201665e7ea0390b765d5
annabmed08 <162120677+annabmed08@users.noreply.github.com>
2025-06-09 09:09:34 -0300

    a

--- main.js
+++ main.js
@@ -23,14 +23,14 @@ contadores[0].textContent = calculaTempo;
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundo= Math.floor(tempoFinal / 1000);
+    let segundos= Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dia = Math.floor(hora / 24);
+    let dias = Math.floor(hora / 24);
 
     segundos %= 60;
-    minuto %= 60;
-    hora %= 24;
+    minutos %= 60;
+    horas %= 24;
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
